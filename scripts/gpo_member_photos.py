f42451ae5baf3159d393ed7b954169120bc33fb9
Kevin Schaul <kevin.schaul@gmail.com>
2024-08-30 16:24:23 -0500

    WIP: Rewrite gpo_member_photos.py to use pictorial
    Still need to fix some missing bioguide ids for 36 legislators, but it's
    a start.
    
    See #221

--- scripts/gpo_member_photos.py
+++ scripts/gpo_member_photos.py
@@ -17,6 +17,8 @@ from urllib.request import urlretrieve
 # pip install -r requirements.txt
 import mechanicalsoup
 
+CURRENT_CONGRESS = 118
+
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
@@ -44,50 +46,97 @@ def pause(last, delay):
     return datetime.datetime.now()
 
 
-def get_photo_list(br, congress_number, delay):
-    last_request_time = None
+def get_members_pictorial(br, congress_number):
+    """
+    Get members for the given congress_number
+    API documentation: https://pictorialapi.gpo.gov/index.html
+    """
+    response = br.get(
+        f"https://pictorialapi.gpo.gov/api/GuideMember/GetMembers/{congress_number}"
+    ).json()
+    return response["memberCollection"]
 
-    page = 1
-    while True:
-        # Fetch a page of results from Congress.gov.
-        print(f"Page {page} of Congress.gov Member listing...")
-        response = br.get(
-            "https://www.congress.gov/search?"
-            + urlencode(
-                {
-                    "q": json.dumps(
-                        {"source": "members", "congress": str(congress_number)}
-                    ),
-                    "pageSize": 250,
-                    "page": page,
-                }
-            )
-        ).text
 
-        if len(response) == 0:
-            sys.exit("Page is blank. Try again later, you may have hit a limit.")
+def get_legislators_current(br, include_historical=False):
+    """
+    Download legislators from sister project unitedstates/congress-legislators
+    Optionally also include historical legislators (which significantly
+    increases the download size)
+    """
+    legislators = br.get(
+        "https://theunitedstates.io/congress-legislators/legislators-current.json"
+    ).json()
+    if include_historical:
+        historical = br.get(
+            "https://theunitedstates.io/congress-legislators/legislators-historical.json"
+        ).json()
+        legislators.append(historical)
+    return legislators
 
-        # Scan for links to Member pages and img tags. The link to the
-        # Congress.gov page uses the Member's Bioguide ID as the key, and the
-        # filename for the photo is the same file name found at
-        # memberguide.gpo.gov for the high-resolution file.
-        for bioguide_id, photo_file in regex1.findall(response):
-            # this part is added by Congress.gov:
-            photo_file = photo_file.replace("_200.jpg", ".jpg")
-            if photo_file == bioguide_id.lower() + ".jpg":
-                continue  # not a file sourced from GPO
-            yield (bioguide_id, photo_file)
 
-        m = regex2.search(response)
-        if m:
-            # fetch next page of results
-            page += 1
-            continue
+def match_bioguide_id(member_pictorial, legislators_current):
+    """
+    Attempt to find the corresponding Bioguide ID for the given member.
+
+    There are many odd cases -- see tests/test_gpo_member_photos.py for
+    examples.
+    """
+    name = member_pictorial["name"]
+
+    common_nicknames = {
+        "Nick": "Nicolas",
+        "Daniel": "Dan",
+        "Mike": "Michael",
+    }
+
+    matches = []
+    for m in legislators_current:
+        # First check whether the name matches
+        name_matches = False
+        m_name_last = m["name"]["last"].replace(" ", "")
+        if m_name_last == member_pictorial["lastName"]:
+            if m["name"]["first"] == member_pictorial["firstName"] or (
+                "nickname" in m["name"]
+                and m["name"]["nickname"] == member_pictorial["firstName"]
+            ):
+                name_matches = True
+            # Sometimes the nickname is encoded in the first name
+            elif member_pictorial["firstName"] in m["name"]["first"]:
+                name_matches = True
+            # Sometimes the nickname is not encoded
+            elif (
+                member_pictorial["firstName"] in common_nicknames
+                and common_nicknames[member_pictorial["firstName"]]
+                == m["name"]["first"]
+            ):
+                name_matches = True
+
+        # The GPO has some first and last names swapped, so check those too
+        if not name_matches and m["name"]["first"] == member_pictorial["lastName"]:
+            if m_name_last == member_pictorial["firstName"] or (
+                "nickname" in m["name"]
+                and m["name"]["nickname"] == member_pictorial["firstName"]
+            ):
+                name_matches = True
+
+        # If the name matches, check the office and state
+        # Note: Assumes we're matching against most recent term
+        if name_matches:
+            most_recent_term = m["terms"][-1]
+            mType = "sen" if member_pictorial["memberType"] == "Senator" else "rep"
+            if (
+                most_recent_term["state"] == member_pictorial["stateId"]
+                and most_recent_term["type"] == mType
+            ):
+                matches.append(m)
+
+    if len(matches) == 1:
+        return matches[0]["id"]["bioguide"]
+    else:
+        if len(matches):
+            raise ValueError(f"Multiple Bioguide ID matches found for {name}")
         else:
-            # this was the last page (no Next link)
-            break
-
-        last_request_time = pause(last_request_time, delay)
+            raise ValueError(f"No Bioguide ID match found for {name}")
 
 
 def save_metadata(bioguide_id):
@@ -97,7 +146,7 @@ def save_metadata(bioguide_id):
     outfile = os.path.join(outdir, bioguide_id + ".yaml")
     with open(outfile, "w") as f:
         f.write("name: GPO Member Guide\n")
-        f.write("link: https://memberguide.gpo.gov\n")
+        f.write("link: https://pictorial.gpo.gov\n")
 
 
 def download_file(url, outfile):
@@ -120,8 +169,7 @@ def download_photos(br, photo_list, outdir, delay):
 
     ok = 0
 
-    for bioguide_id, photo_filename in photo_list:
-        photo_url = "https://memberguide.gpo.gov/PictorialImages/" + photo_filename
+    for bioguide_id, photo_url in photo_list:
         print(bioguide_id, photo_url)
 
         filename = os.path.join(outdir, bioguide_id + ".jpg")
@@ -148,14 +196,15 @@ def resize_photos():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Scrape https://memberguide.gpo.gov and save "
-        "members' photos named after their Bioguide IDs",
+        description="Save members' photos from pictorialapi.gpo.gov, named "
+        "after their Bioguide IDs",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "-n",
         "--congress",
-        default="114",
+        default=CURRENT_CONGRESS,
+        type=int,
         help="Congress session number, for example: 110, 111, 112, 113",
     )
     parser.add_argument(
@@ -183,11 +232,28 @@ if __name__ == "__main__":
     br = mechanicalsoup.Browser()
     br.set_user_agent(USER_AGENT)
 
-    photo_list = get_photo_list(br, args.congress, args.delay)
+    legislators_current = get_legislators_current(br, args.congress < CURRENT_CONGRESS)
+    members_pictorial = get_members_pictorial(br, args.congress)
+
+    no_bioguide_match = []
+    photo_list = []
+    for m in members_pictorial:
+        try:
+            photo_list.append(
+                (match_bioguide_id(m, legislators_current), m["imageUrl"])
+            )
+        except ValueError as e:
+            print(e)
+            no_bioguide_match.append(m)
 
     number = download_photos(br, photo_list, args.outdir, args.delay)
 
     if number:
         resize_photos()
+
+    if len(no_bioguide_match):
+        print(f"Missing bioguide matches for {len(no_bioguide_match)}, wrote to missing-bioguide-match.json")
+        with open("missing-bioguide-match.json", "w") as f:
+            json.dump(no_bioguide_match, f, indent=2)
 
 # End of file
--- test/test_gpo_member_photos.py
+++ test/test_gpo_member_photos.py
@@ -43,6 +43,453 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertGreaterEqual(time2 - time1, delta)
 
 
+class TestMatchBioguideID(unittest.TestCase):
+    def test_basic(self):
+        member_pictorial = {
+            "entryId": "13421",
+            "memberId": 13421,
+            "memberType": "Senator",
+            "lastName": "Booker",
+            "firstName": "Cory",
+            "name": "Booker, Cory A.",
+            "partyDescription": "Democrat",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "NJ",
+            "stateDescription": "New Jersey",
+            "memberTypeId": "SR",
+            "district": 0,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_NJ_Booker_Cory.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_NJ_Booker_Cory.jpg",
+            "imageFile": "118_SR_NJ_Booker_Cory.jpg",
+        }
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "B001288",
+                    "lis": "S370",
+                    "thomas": "02194",
+                    "govtrack": 412598,
+                    "opensecrets": "N00035267",
+                    "votesmart": 76151,
+                    "wikipedia": "Cory Booker",
+                    "ballotpedia": "Cory Booker",
+                    "fec": ["S4NJ00185"],
+                    "cspan": 84679,
+                    "maplight": 2051,
+                    "wikidata": "Q1135767",
+                    "google_entity_id": "kg:/m/06p430",
+                    "icpsr": 41308,
+                },
+                "name": {
+                    "first": "Cory",
+                    "middle": "Anthony",
+                    "last": "Booker",
+                    "official_full": "Cory A. Booker",
+                },
+                "bio": {"birthday": "1969-04-27", "gender": "M"},
+                "leadership_roles": [
+                    {
+                        "title": "Senate Democratic Policy & Communications Committee Vice Chair",
+                        "chamber": "senate",
+                        "start": "2023-01-03",
+                    }
+                ],
+                "terms": [
+                    {
+                        "type": "sen",
+                        "start": "2013-10-31",
+                        "end": "2015-01-03",
+                        "state": "NJ",
+                        "class": 2,
+                        "party": "Democrat",
+                        "state_rank": "junior",
+                        "url": "http://www.booker.senate.gov",
+                        "phone": "202-224-3224",
+                        "address": "141 Hart Senate Office Building Washington DC 20510",
+                        "office": "141 Hart Senate Office Building",
+                        "contact_form": "http://www.booker.senate.gov/?p=contact",
+                    },
+                    {
+                        "type": "sen",
+                        "start": "2015-01-06",
+                        "end": "2021-01-03",
+                        "state": "NJ",
+                        "class": 2,
+                        "party": "Democrat",
+                        "state_rank": "junior",
+                        "url": "https://www.booker.senate.gov",
+                        "phone": "202-224-3224",
+                        "address": "717 Hart Senate Office Building Washington DC 20510",
+                        "office": "717 Hart Senate Office Building",
+                        "contact_form": "https://www.booker.senate.gov/?p=contact",
+                        "fax": "202-224-8378",
+                    },
+                    {
+                        "type": "sen",
+                        "start": "2021-01-03",
+                        "end": "2027-01-03",
+                        "state": "NJ",
+                        "class": 2,
+                        "state_rank": "junior",
+                        "party": "Democrat",
+                        "url": "https://www.booker.senate.gov",
+                        "contact_form": "https://www.booker.senate.gov/?p=contact",
+                        "address": "717 Hart Senate Office Building Washington DC 20510",
+                        "office": "717 Hart Senate Office Building",
+                        "phone": "202-224-3224",
+                    },
+                ],
+            }
+        ]
+        expected = "B001288"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+    def test_hassan(self):
+        """First name is a nickname"""
+        member_pictorial = {
+            "entryId": "13426",
+            "memberId": 13426,
+            "memberType": "Senator",
+            "lastName": "Hassan",
+            "firstName": "Maggie",
+            "name": "Hassan, Maggie",
+            "partyDescription": "Democrat",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "NH",
+            "stateDescription": "New Hampshire",
+            "memberTypeId": "SR",
+            "district": 0,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_NH_Hassan_Maggie.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_NH_Hassan_Maggie.jpg",
+            "imageFile": "118_SR_NH_Hassan_Maggie.jpg",
+        }
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "H001076",
+                    "fec": ["S6NH00091"],
+                    "govtrack": 412680,
+                    "votesmart": 42552,
+                    "wikipedia": "Maggie Hassan",
+                    "ballotpedia": "Maggie Hassan",
+                    "lis": "S388",
+                    "wikidata": "Q24053",
+                    "google_entity_id": "kg:/m/03c3zch",
+                    "opensecrets": "N00038397",
+                    "maplight": 2192,
+                    "cspan": 67481,
+                    "icpsr": 41702,
+                },
+                "name": {
+                    "first": "Margaret",
+                    "middle": "Wood",
+                    "nickname": "Maggie",
+                    "last": "Hassan",
+                    "official_full": "Margaret Wood Hassan",
+                },
+                "bio": {"gender": "F", "birthday": "1958-02-27"},
+                "terms": [
+                    {
+                        "type": "sen",
+                        "start": "2017-01-03",
+                        "end": "2023-01-03",
+                        "state": "NH",
+                        "class": 3,
+                        "state_rank": "junior",
+                        "party": "Democrat",
+                        "address": "324 Hart Senate Office Building Washington DC 20510",
+                        "office": "324 Hart Senate Office Building",
+                        "phone": "202-224-3324",
+                        "url": "https://www.hassan.senate.gov",
+                        "contact_form": "https://www.hassan.senate.gov/content/contact-senator",
+                    },
+                    {
+                        "type": "sen",
+                        "start": "2023-01-03",
+                        "end": "2029-01-03",
+                        "state": "NH",
+                        "class": 3,
+                        "state_rank": "junior",
+                        "party": "Democrat",
+                        "url": "https://www.hassan.senate.gov",
+                        "contact_form": "https://www.hassan.senate.gov/content/contact-senator",
+                        "address": "324 Hart Senate Office Building Washington DC 20510",
+                        "office": "324 Hart Senate Office Building",
+                        "phone": "202-224-3324",
+                    },
+                ],
+            }
+        ]
+        expected = "H001076"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+    def test_hickenlooper(self):
+        """GPO has first and last names swapped :("""
+        member_pictorial = {
+            "entryId": "13433",
+            "memberId": 13433,
+            "memberType": "Senator",
+            "lastName": "John",
+            "firstName": "Hickenlooper",
+            "name": "John, Hickenlooper",
+            "partyDescription": "Democrat",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "CO",
+            "stateDescription": "Colorado",
+            "memberTypeId": "SR",
+            "district": 0,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_CO_John_Hickenlooper.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_SR_CO_John_Hickenlooper.jpg",
+            "imageFile": "118_SR_CO_John_Hickenlooper.jpg",
+        }
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "H000273",
+                    "lis": "S408",
+                    "fec": ["S0CO00575"],
+                    "opensecrets": "N00044206",
+                    "govtrack": 456797,
+                    "wikidata": "Q430518",
+                    "wikipedia": "John Hickenlooper",
+                    "google_entity_id": "kg:/m/04g_1z",
+                    "ballotpedia": "John Hickenlooper",
+                },
+                "name": {
+                    "first": "John",
+                    "middle": "Wright",
+                    "last": "Hickenlooper",
+                    "official_full": "John W. Hickenlooper",
+                },
+                "bio": {"gender": "M", "birthday": "1952-02-07"},
+                "terms": [
+                    {
+                        "type": "sen",
+                        "start": "2021-01-03",
+                        "end": "2027-01-03",
+                        "state": "CO",
+                        "class": 2,
+                        "state_rank": "junior",
+                        "party": "Democrat",
+                        "url": "https://www.hickenlooper.senate.gov",
+                        "contact_form": "https://www.hickenlooper.senate.gov/contact/",
+                        "address": "374 Russell Senate Office Building Washington DC 20510",
+                        "office": "374 Russell Senate Office Building",
+                        "phone": "202-224-5941",
+                    }
+                ],
+            }
+        ]
+        expected = "H000273"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+    def test_edwards(self):
+        """Chuck Edwards has nickname coded differently"""
+        member_pictorial = {
+            "entryId": "13140",
+            "memberId": 13140,
+            "memberType": "Representative",
+            "lastName": "Edwards",
+            "firstName": "Chuck",
+            "name": "Edwards, Chuck",
+            "partyDescription": "Republican",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "NC",
+            "stateDescription": "North Carolina",
+            "memberTypeId": "RP",
+            "district": 11,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NC_11_Edwards_Chuck.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NC_11_Edwards_Chuck.jpg",
+            "imageFile": "118_RP_NC_11_Edwards_Chuck.jpg",
+        }
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "E000246",
+                    "fec": ["H2NC14050"],
+                    "govtrack": 456914,
+                    "opensecrets": "N00049670",
+                    "wikidata": "Q55065043",
+                    "wikipedia": "Chuck Edwards",
+                    "google_entity_id": "kg:/g/11ghxw1t29",
+                    "votesmart": 166600,
+                },
+                "name": {
+                    "first": "Charles (Chuck)",
+                    "middle": "Marion",
+                    "last": "Edwards",
+                    "official_full": "Chuck Edwards",
+                },
+                "bio": {"gender": "M", "birthday": "1960-09-13"},
+                "terms": [
+                    {
+                        "type": "rep",
+                        "start": "2023-01-03",
+                        "end": "2025-01-03",
+                        "state": "NC",
+                        "district": 11,
+                        "party": "Republican",
+                        "url": "https://edwards.house.gov",
+                        "address": "1505 Longworth House Office Building Washington DC 20515-3311",
+                        "office": "1505 Longworth House Office Building",
+                        "phone": "202-225-6401",
+                    }
+                ],
+            }
+        ]
+        expected = "E000246"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+    def test_leger_fernandez(self):
+        """Leger Fernandez has no space in GPO"""
+        member_pictorial = {
+            "entryId": "13164",
+            "memberId": 13164,
+            "memberType": "Representative",
+            "lastName": "LegerFernandez",
+            "firstName": "Teresa",
+            "name": "LegerFernandez, Teresa",
+            "partyDescription": "Democrat",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "NM",
+            "stateDescription": "New Mexico",
+            "memberTypeId": "RP",
+            "district": 3,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NM_3_LegerFernandez_Teresa.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NM_3_LegerFernandez_Teresa.jpg",
+            "imageFile": "118_RP_NM_3_LegerFernandez_Teresa.jpg",
+        }
+
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "L000273",
+                    "fec": ["H0NM03102"],
+                    "opensecrets": "N00044559",
+                    "govtrack": 456835,
+                    "wikidata": "Q96054905",
+                    "wikipedia": "Teresa Leger Fernandez",
+                    "google_entity_id": "kg:/g/11h_x7n8f5",
+                },
+                "name": {
+                    "first": "Teresa",
+                    "last": "Leger Fernandez",
+                    "official_full": "Teresa Leger Fernandez",
+                },
+                "bio": {"gender": "F", "birthday": "1959-07-01"},
+                "terms": [
+                    {
+                        "type": "rep",
+                        "start": "2021-01-03",
+                        "end": "2023-01-03",
+                        "state": "NM",
+                        "district": 3,
+                        "party": "Democrat",
+                        "address": "1432 Longworth House Office Building Washington DC 20515-3103",
+                        "office": "1432 Longworth House Office Building",
+                        "phone": "202-225-6190",
+                        "url": "https://fernandez.house.gov",
+                    },
+                    {
+                        "type": "rep",
+                        "start": "2023-01-03",
+                        "end": "2025-01-03",
+                        "state": "NM",
+                        "district": 3,
+                        "party": "Democrat",
+                        "url": "https://fernandez.house.gov",
+                        "address": "1510 Longworth House Office Building Washington DC 20515-3103",
+                        "office": "1510 Longworth House Office Building",
+                        "phone": "202-225-6190",
+                    },
+                ],
+            }
+        ]
+        expected = "L000273"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+    def test_lalota(self):
+        """LaLota is Nick and Nicholas"""
+        member_pictorial = {
+            "entryId": "13169",
+            "memberId": 13169,
+            "memberType": "Representative",
+            "lastName": "LaLota",
+            "firstName": "Nick",
+            "name": "LaLota, Nick",
+            "partyDescription": "Republican",
+            "congressionalDescription": "118th Congress",
+            "title": "Not Applicable",
+            "stateId": "NY",
+            "stateDescription": "New York",
+            "memberTypeId": "RP",
+            "district": 1,
+            "thumbNailImageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NY_1_LaLota_Nick.jpg",
+            "imageUrl": "https://memberguide.gpo.gov/pictorialimages/118_RP_NY_1_LaLota_Nick.jpg",
+            "imageFile": "118_RP_NY_1_LaLota_Nick.jpg",
+        }
+
+        legislators_current = [
+            {
+                "id": {
+                    "bioguide": "L000598",
+                    "fec": ["H2NY01190"],
+                    "govtrack": 456920,
+                    "opensecrets": "N00050419",
+                    "wikidata": "Q115168182",
+                    "wikipedia": "Nick LaLota",
+                    "votesmart": 191957,
+                },
+                "name": {
+                    "first": "Nicolas",
+                    "middle": "Joseph",
+                    "last": "LaLota",
+                    "official_full": "Nick LaLota",
+                },
+                "bio": {"gender": "M", "birthday": "1978-06-23"},
+                "terms": [
+                    {
+                        "type": "rep",
+                        "start": "2023-01-03",
+                        "end": "2025-01-03",
+                        "state": "NY",
+                        "district": 1,
+                        "party": "Republican",
+                        "url": "https://lalota.house.gov",
+                        "address": "1530 Longworth House Office Building Washington DC 20515-3201",
+                        "office": "1530 Longworth House Office Building",
+                        "phone": "202-225-3826",
+                    }
+                ],
+            }
+        ]
+        expected = "L000598"
+        self.assertTrue(
+            gpo_member_photos.match_bioguide_id(member_pictorial, legislators_current)
+            == expected
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
 
